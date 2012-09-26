94b7405116ae5d50d6b3daf54cf6f1d5386afc8e
David Reed <dreed@thoughtworks.com>
2012-09-26 16:04:07 +0200

    Removing unneeded dependencies

--- src/test/integration/java/your_package/web/views/ViewTest.java
+++ src/test/integration/java/your_package/web/views/ViewTest.java
@@ -11,6 +11,7 @@ import org.springframework.web.servlet.ViewResolver;
 import javax.annotation.Resource;
 import java.util.Locale;
 
+// It would be good if we could replace this with a 'spring-test'/'spring-test-mvc' class to remove the dependency on 'javax.servlet:javax.servlet-api:+'
 @ContextConfiguration(locations = {"classpath:spring/mvc-config.xml"}, loader = MockServletContextLoader.class)
 public abstract class ViewTest extends AbstractJUnit4SpringContextTests {
 
