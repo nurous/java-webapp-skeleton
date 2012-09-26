39bd36eb21efb6beaa46409b6cf35404db4ea2ee
David Reed <dreed@thoughtworks.com>
2012-09-26 17:53:14 +0200

    Ensuring all tests run and pass in initial state

--- src/test/functional/java/SmokeTest.java
+++ src/test/functional/java/SmokeTest.java
@@ -23,16 +23,15 @@ public class SmokeTest {
     }
 
     @Test
-    @Ignore
     public void thingy_CRUD() {
-        // TODO: 1: Use this test to drive creation of the CRUD operations for a 'Thingy'
-
         driver.get("http://localhost:8080/YOUR_APP_NAME/thingy");
         driver.findElement(By.linkText("New Thingy")).click();
         driver.findElement(By.name("name")).sendKeys("Bob's Amazing Widget");
         driver.findElement(By.xpath("//input[@type='submit']")).click();
         assertEquals("Bob's Amazing Widget", driver.findElement(By.cssSelector("#name")).getText());
-        driver.findElement(By.linkText("edit")).click();
+
+        // TODO: 1: Use this test to drive creation of the CRUD operations for a 'Thingy'
+        //driver.findElement(By.linkText("edit")).click();
 
         // TODO: 4: Learn about the page object pattern and how that could help improve maintainability of this type of test.
     }
